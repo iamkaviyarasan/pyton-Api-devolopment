6585bd5780610fc807411a5148b5d10d539fa118
iamkaviyarasan <kaviyarasangunasekaran@gmail.com>
2024-04-03 17:22:04 +0530

    7.37

--- fastapi/app/oauth2.py
+++ fastapi/app/oauth2.py
@@ -1,5 +1,6 @@
 from jose import JWTError, jwt
-from datetime import timedelta,datetime
+from datetime import timedelta
+import datetime
 from . import schemas
 from fastapi import Depends,status
 from fastapi import HTTPException
@@ -13,11 +14,11 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES =1
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes= ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.datetime.utcnow() + timedelta(minutes= ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -33,8 +34,7 @@ def verify_acesss_token(token:str,credentials_exception):
         raise credentials_exception
     
       token_data = schemas.TokenData(id=str(id))
-    except JWTError as e:
-        print(e)
+    except JWTError :
         raise credentials_exception
     
     return token_data
