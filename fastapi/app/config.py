63105766c7eb78c39260d976e68659176ae464c1
iamkaviyarasan <kaviyarasangunasekaran@gmail.com>
2024-04-09 12:53:58 +0530

    14.04

--- fastapi/app/config.py
+++ fastapi/app/config.py
@@ -1,24 +1,14 @@
-# Old import statement
-# from pydantic import BaseSettings
-
-# New import statement
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_password: str
-    database_name: str
-    database_username: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+     database_hostname: str
+     database_port : str
+     database_password : str
+     database_name : str
+     database_username : str
+     secret_key : str
+     algorithm : str
+     access_token_expire_minutes : int
 
-    class Config:
-        env_file = ".env"
-        # env_file_encoding = "utf-8"
-        # env_file_encoding = "utf-8"
-        # env_file_encoding = "utf-8"
-
-settings = Settings()
+          
+settings = Settings(_env_file='.env')
